2a882bc51e202c80878dd57f6a788c6d12e31762
VaskoEK83 <vaskoeszterklara@gmail.com>
2023-03-20 21:29:34 +0100

    masodik_hazi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_vk';
+
+  inputByUser: string = "";
+  array: number[] = [];
+  activeImageNumber: number = 0;
+
+
+  inputChange1(): void {
+
+    console.log(this.inputByUser);
+
+    this.array = [];
+
+    if (Number(parseInt(this.inputByUser)) <= 0 ) {
+      this.inputByUser = "1";
+    }
+    if (Number(parseInt(this.inputByUser)) >= 4) {
+      this.inputByUser = "4";
+    }
+
+    for (let i = 1; i <= Number(this.inputByUser); i++) {
+      this.array.push(i);
+    }
+    console.log(this.array);
+
+    
+    this.activeImageNumber = 1;
+  }
+
+  leftButtonClicked(): void {
+
+    let pic_nr = this.activeImageNumber - 1;
+            
+    if (pic_nr < this.array[0]) {
+      this.activeImageNumber = this.array[this.array.length - 1];
+    }
+    else {
+      this.activeImageNumber = pic_nr;
+    }
+  }
+
+  rightButtonClicked(): void {
+
+    let pic_nr = this.activeImageNumber + 1;
+            
+    if (pic_nr > this.array[this.array.length - 1]) {
+      this.activeImageNumber = 1;
+    }
+    else {
+      this.activeImageNumber = pic_nr;
+    }
+  }
 }
