42cfd61e2d8dcc043d0051cd0bc150d58725d8ce
VaskoEK83 <vaskoeszterklara@gmail.com>
2023-05-02 14:36:45 +0200

    mod0502

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './feature/home/home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
--- src/app/feature/home/home.component.spec.ts
+++ src/app/feature/home/home.component.spec.ts
@@ -1,23 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [RouterTestingModule.withRoutes([])]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router = TestBed.get(Router); // TestBed.inject(Router) for Angular 9+
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+
+
+  https://stackoverflow.com/questions/39791773/how-can-i-unit-test-a-component-that-uses-the-router-in-angular
+
+  it('should navigate', () => {
+    
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector('#buttonForNavOneProd');
+    const navigateSpy = spyOn(router, 'navigate');  // router fentebb (11. és 24. sor) definiálva
+
+    button.click();
+    expect(navigateSpy).toHaveBeenCalledWith(['/product', '/:id']);
+
+  });
+    
+
+
 });
